413b80d39ffaa211b0f7c3ec5c75c4338d463ec9
Laksisi Chetha Gunaratne <90137011+Laksisi99@users.noreply.github.com>
2023-03-06 22:00:56 +0530

    get request

--- axios-practice-app/src/App.js
+++ axios-practice-app/src/App.js
@@ -1,10 +1,48 @@
 import './App.css';
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const[data,setData] = useState([])
+
+  useEffect(() => {
+    Axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res => {
+      console.log("Getting from ::::",res.data)
+      setData(res.data)
+    }).catch(err => console.log(err))
+  }, [])
+
+  const arr = data.map((data, index) => {
+    return(
+      <tr>
+        <td>{data.id}</td>
+        <td>{data.title}</td>
+        <td>{data.body}</td>
+      </tr>
+    )
+  })
+
   return (
-    <div >
-      Axios - Library
-      post,get,update,delete
+    <div className='App'>
+
+     <h1>Lets Use Axios with React js</h1>
+
+    <table>
+
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Body</th>
+      </tr>
+
+      {arr}
+      
+      
+    </table>
+
+
     </div>
   );
 }
